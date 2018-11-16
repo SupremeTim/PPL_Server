9c9a6a24480120fb779ab57edf880cca9e546817
InTaek <dlsxor21c@naver.com>
2018-11-16 23:12:33 +0900

    Login func complete

--- routes/home.js
+++ routes/home.js
@@ -1,4 +1,5 @@
-const express=require('exrpess');
+const express=require('express');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 
 const router=express.Router();
 
@@ -6,4 +7,23 @@ router.get('/',(req,res,next)=>{
     res.render('',{});
 });
 
+router.get('/login',isNotLoggedIn,(req,res,next)=>{
+    res.render('',{});
+});
+
+router.get('/product',(req,res,next)=>{
+    res.render('',{});
+});
+
+router.get('/make',(req,res,next)=>{
+    res.render('',{});
+});
+
+router.get('/search',(req,res,next)=>{
+    res.render('',{});
+});
+
+router.get('/setting',(req,res,next)=>{
+    res.render('',{});
+});
 module.exports=router;
