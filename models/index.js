4dd7bf63a999c615217f8aecaab9dd2cf3aea027
giantim <giantim132@gmail.com>
2018-11-23 16:04:50 +0900

    sourceKey -> targetKey

--- models/index.js
+++ models/index.js
@@ -28,25 +28,26 @@ db.Portfolio.belongsTo(db.User, {foreignKey: 'user_id', targetKey: 'id'});
 
 // portfolio 1 : 1 info - 포트폴리오 하나 당 입력하는 회원 정보는 하나다
 db.Portfolio.hasOne(db.Info, {foreignKey: 'port_id', sourceKey: 'id'});
-db.Info.belongsTo(db.User, {foreignKey: 'port_id', sourceKey: 'id'});
+db.Info.belongsTo(db.User, {foreignKey: 'port_id', targetKey: 'id'});
 
 // info 1 : 1 pro_ex - 포트폴리오 하나 당 프로젝트 경험 하나를 입력받으므로 입력 정보와 경험의 관계는 1:1
 db.Info.hasOne(db.Project_Experience, {foreignKey: 'info_id', sourceKey: 'id'});
-db.Project_Experience.belongsTo(db.Info, {foreignKey: 'info_id', sourceKey: 'id'});
+db.Project_Experience.belongsTo(db.Info, {foreignKey: 'info_id', targetKey: 'id'});
 
 // portfolio 1 : 1 template - 포트폴리오는 하나의 템플릿으로 구성되므로 1:1
 db.Info.hasOne(db.Template, {foreignKey: 'port_id', sourceKey: 'id'});
-db.Template.belongsTo(db.Info, {foreignKey: 'port_id', sourceKey: 'id'});
+db.Template.belongsTo(db.Info, {foreignKey: 'port_id', targetKey: 'id'});
 
 // portfolio 1 : 1 category_header - 포트폴리오 하나는 카테고리 묶음 하나를 갖는다
 db.Portfolio.hasOne(db.Category_Header, {foreignKey: 'port_id', sourceKey: 'id'});
-db.Category_Header.belongsTo(db.Portfolio, {foreignKey: 'port_id', sourceKet: 'id'});
+db.Category_Header.belongsTo(db.Portfolio, {foreignKey: 'port_id', targetKey: 'id'});
 
 // category_header 1 : 1 dev_field, spe_field, dev_lang - 카테고리 묶음은 개발 분야, 전문 분야, 개발 언어를 하나씩 갖는다
 db.Category_Header.hasOne(db.Dev_Field, {foreignKey: 'category_id', sourceKey: 'id'});
-db.Dev_Field.belongsTo(db.Category_Header, {foreignKey: 'category_id', sourceKey: 'id'});
+db.Dev_Field.belongsTo(db.Category_Header, {foreignKey: 'category_id', targetKey: 'id'});
 db.Category_Header.hasOne(db.Spe_Field, {foreignKey: 'category_id', sourceKey: 'id'});
-db.Spe_Field.belongsTo(db.Category_Header, {foreignKey: 'category_id', sourceKey: 'id'});
+db.Spe_Field.belongsTo(db.Category_Header, {foreignKey: 'category_id', targetKey: 'id'});
 db.Category_Header.hasOne(db.Dev_Lang, {foreignKey: 'category_id', sourceKey: 'id'});
+db.Dev_Lang.belongsTo(db.Category_Header, {foreignKey: 'category_id', targetKey: 'id'});
 
 module.exports = db;
