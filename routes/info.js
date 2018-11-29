a4fc7424e8e250b100bf826c85d0d13b15ffef8c
InTaek <dlsxor21c@naver.com>
2018-11-29 18:22:49 +0900

    [ing]Modify info router

--- routes/info.js
+++ routes/info.js
@@ -2,9 +2,9 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const { isLoggedIn, isNotLoggedIn ,upload} = require('./middlewares');
+const { isLoggedIn, isNotLoggedIn, upload } = require('./middlewares');
 
-const { Info,Portfolio } = require('../models');
+const { Info, Portfolio } = require('../models');
 const router = express.Router();
 
 fs.readdir('uploads', (error) => {
@@ -14,20 +14,23 @@ fs.readdir('uploads', (error) => {
     }
 });
 
-router.get('/',(req,res,next)=>{
-    Portfolio.create({
-        views:0,
-        user_id:req.user.id,
-    });
-});                                     // 시작하기 버튼 누를시
+router.get('/', isLoggedIn, async (req, res, next) => {
+    try {
+        await Portfolio.create({
+            views: 0,
+            user_id: req.user.id,
+        });
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
 
-router.get('/standard', isLoggedIn, (req, res, next) => {
     res.render('', {
         name: req.user.name,
         phone: req.user.phone,
         email: req.user.email,
     });                         // 프론트랑 맞추기, 기본정보 출력
-});
+});                                     // 시작하기 버튼 누를시
 
 router.post('/img', upload.single('img'), (req, res) => {
     res.json({ url: `/img/${req.file.filename}` });
@@ -35,6 +38,7 @@ router.post('/img', upload.single('img'), (req, res) => {
 
 router.post('/submit', async (req, res, next) => {
     try {
+        await Portfolio.                    // 검색해서 id 가져오도록 설정해보자!
         await Info.create({              // form 태그의 name 속성 정해주기
             profile_image: req.body.url,
             open_age: req.body.age,
