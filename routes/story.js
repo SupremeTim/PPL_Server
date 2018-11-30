6f3a34b847bbc63472613774901a54eb59a4f267
InTaek <dlsxor21c@naver.com>
2018-11-30 12:56:14 +0900

    Modify story router

--- routes/story.js
+++ routes/story.js
@@ -2,12 +2,22 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const { isLoggedIn, isNotLoggedIn,upload } = require('./middlewares');
-
+const { Portfolio, Project_experience, Dev_field, Dev_Lang, Spe_Field } = require('../models');
+const { isLoggedIn, isNotLoggedIn, upload } = require('./middlewares');
+var portId;
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.render('', {});                        // 기본정보에서 다음 누르고 난뒤 프론트파일 보여주기
+router.get('/', isLoggedIn, (req, res, next) => {
+    portId = await Portfolio.find({
+        attributes: 'id',            // 포트폴리오 id를 가져옴
+        where: {
+            user_id: req.user.id,    // 현재 유저가 만든 
+        },
+        order: [['id', 'DESC']],      // 유저가 만든 포트폴리오 중에 가장 뒤에 있는 것이 방금 만든 포트폴리오이므로
+    });  
+    res.render('', {
+        name: req.user.name,     // 오른쪽 위에 이름 출력
+    });                        // 기본정보에서 다음 누르고 난뒤 프론트파일 보여주기
 });
 
 fs.readdir('uploads', (error) => {
@@ -21,15 +31,16 @@ router.post('/img', upload.single('img'), (req, res) => {
     res.json({ url: `/img/${req.file.filename}` });
 });
 
-router.post('/exp', async (req, res, next) => {
+router.post('/exp', async (req, res, next) => {     // 경험박스마다 저장이 있었으면 좋겠음
     try {
-        await project_experience.create({
+        await Project_experience.create({
             pro_name: req.body.name,
             pro_comment: req.body.comment,
             pro_link: req.body.link,
             pro_image: req.body.url,
-        });                                                             // 프론트랑 맞추기
-        res.redirect('/');
+            port_id: portId,
+        });                          // 프론트랑 맞추기
+        res.send('Save experience complete!')
     } catch (error) {
         console.error(error);
         next(error);
@@ -38,20 +49,20 @@ router.post('/exp', async (req, res, next) => {
 
 router.post('/submit', async (req, res, next) => {
     try {
-        await dev_field.create({
-            android:,
-            ios:,
-            web:,
-            server:,
+        await Dev_field.create({
+            android:req.body.data1[0],
+            ios:req.body.data1[1],
+            web:req.body.data1[2],
+            server:req.body.data1[3],
         });
-        await dev_lang.create({
+        await Dev_Lang.create({
             c:,
             cpp:,
             csharp:,
             java:,
             python:,
         });
-        await spe_field.create({
+        await Spe_Field.create({
             secure:,
             iot:,
             ai:,
