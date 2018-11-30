69b0e293e5c9fbda1acc1caecb7dc5e40d267542
giantim <giantim132@gmail.com>
2018-11-30 19:41:06 +0900

    modify portfolio router

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -10,7 +10,25 @@ router.get('/',(req,res,next)=>{
 });     
 
 router.get('/submit',(req,res,next)=>{
-    res.redirect();                         // 완성 페이지로 넘어가기
+    try{
+        const portId = await Portfolio.find({
+            attributes: 'id',            // 포트폴리오 id를 가져옴
+            where: {
+                user_id: req.user.id,    // 현재 유저가 만든 
+            },
+            order: [['id', 'DESC']],      // 유저가 만든 포트폴리오 중에 가장 뒤에 있는 것이 방금 만든 포트폴리오이므로
+        });  
+        await Portfolio.update({
+            // url도 추가해야됨
+            port_name: req.body.port_name,
+        }, {
+            where: { id: port_id},
+        });
+        res.send('Success!');                         // 완성 페이지로 넘어가기
+    }
+    catch(Exception e) {
+        console.log(e);
+    }
 });
 
 module.exports=router;
