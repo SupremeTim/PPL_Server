67588dbe766871ff3211aeda717ec9deaf2a2bf9
InTaek <dlsxor21c@naver.com>
2018-12-05 23:09:10 +0900

    Add coloum img

--- models/portfolio.js
+++ models/portfolio.js
@@ -7,11 +7,16 @@ module.exports = (sequelize, DataTypes) => (
         port_views: {
             type: DataTypes.INTEGER,
             allowNull: false,
+            defaultValue:0,
         },
         port_name: {
             type: DataTypes.STRING(20),
             allowNull: true,
             defaultValue: "",
+        },
+        port_img:{
+            type:DataTypes.STRING(100),
+            allowNull:true,
         },
         dev_field: {
             type: DataTypes.STRING(50),
