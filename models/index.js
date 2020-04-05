cc43c213c7484ed78ac53797c19b1c1d0d5b9b07
In Taek Cho <dlsxor21c@naver.com>
2020-04-05 21:56:50 +0900

    Refactoring: edit a comment

--- models/index.js
+++ models/index.js
@@ -31,11 +31,11 @@ db.Career_Detail.belongsTo(db.Portfolio, { foreignKey: 'port_id', targetKey: 'id
 db.Portfolio.hasOne(db.Info, {foreignKey: 'port_id', sourceKey: 'id'});
 db.Info.belongsTo(db.Portfolio, {foreignKey: 'port_id', targetKey: 'id'});
 
-// portfolio 1 : 1 pro_ex - 포트폴리오 하나 당 프로젝트 경험 하나를 입력받으므로 입력 정보와 경험의 관계는 1:1
+// portfolio 1 : N pro_ex - 포트폴리오 하나 당 프로젝트 경험 여러 개를 입력받으므로
 db.Portfolio.hasMany(db.Project_Experience, {foreignKey: 'port_id', sourceKey: 'id'});
 db.Project_Experience.belongsTo(db.Portfolio, {foreignKey: 'port_id', targetKey: 'id'});
 
-// portfolio 1 : 1 template - 포트폴리오는 하나의 템플릿으로 구성되므로 1:1
+// portfolio 1 : N template - 포트폴리오 하나당 템플릿 여러개 -> 수정이 필요할 듯?
 db.Portfolio.hasMany(db.Template, {foreignKey: 'port_id', sourceKey: 'id'});
 db.Template.belongsTo(db.Portfolio, {foreignKey: 'port_id', targetKey: 'id'});
 
